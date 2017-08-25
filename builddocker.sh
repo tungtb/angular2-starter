c424c1667c54981311f833054e373744e88252e3
TungTB <tranbatungbk@gmail.com>
2017-08-25 14:16:00 +0700

    Fixing docker build

--- builddocker.sh
+++ builddocker.sh
@@ -1,2 +1,4 @@
 sh builddist.sh
-docker build -t tranbatungbk/angular2-starter .
+dateVersion=$(date +"%Y%m%d")
+docker build -t tranbatungbk/angular2-starter:$dateVersion .
+docker push tranbatungbk/angular2-starter:$dateVersion
